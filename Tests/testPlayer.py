87970c075a118600956b4d9935c8702c0c42b255
katafalk <katafalk2009@gmail.com>
2021-07-05 19:23:28 +0300

    Added test_board.py

--- Tests/testPlayer.py
+++ Tests/testPlayer.py
@@ -23,12 +23,4 @@ def rum():
     return RUM(500, 500, r'D:\Pycharm Projects\Firstgame\rum.png')
 
 
-def test_rum_heals(test_player,rum):
-    test_player.x=500
-    test_player.y=500
-    test_player.collide_rum(rum)
-    assert test_player.health==4
 
-def spike_platform_hurts(test_player,spike_platform):
-    test_player.x = 500
-    test_player.y = 500
